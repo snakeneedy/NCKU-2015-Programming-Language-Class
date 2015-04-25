a602d52806ebf489225162e9c826045627c7f0f3
snakeneedy <snakeneedy@gmail.com>
2015-04-25 08:48:04 +0800

    U: 精減程式碼 => 39 行

--- hw02/hw02.lisp
+++ hw02/hw02.lisp
@@ -1,36 +1,3 @@
-;function mergesort(m)
-;   var list left, right, result
-;   if length(m) ≤ 1
-;       return m
-;   else
-;       var middle = length(m) / 2
-;       for each x in m up to middle - 1
-;           add x to left
-;       for each x in m at and after middle
-;           add x to right
-;       left = mergesort(left)
-;       right = mergesort(right)
-;       if last(left) ≤ first(right) 
-;          append right to left
-;          return left
-;       result = merge(left, right)
-;       return result
-;
-;function merge(left,right)
-;   var list result
-;   while length(left) > 0 and length(right) > 0
-;       if first(left) ≤ first(right)
-;           append first(left) to result
-;           left = rest(left)
-;       else
-;           append first(right) to result
-;           right = rest(right)
-;   if length(left) > 0 
-;       append rest(left) to result
-;   if length(right) > 0 
-;       append rest(right) to result
-;   return result
-;
 (defun readList (N)
 	(loop for i from 1 to N collect (read)) 
 )
@@ -69,11 +36,4 @@
 	)
 )
 ; main ()
-(
-	let ( (N (read)) )
-	(
-		let ( (lst (readList N)) )
-		(format t "~A~%" (mergesort lst))
-	)
-	nil
-)
+(format t "~A~%" (mergesort (readList (read))))
